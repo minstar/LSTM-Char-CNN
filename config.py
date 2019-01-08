fe78b98978dcb10ad756ac555d3b2b43ebf25e6f
minstar <wjdalsquf@gmail.com>
2019-01-08 17:55:27 +0900

    model done except softmax and cross entropy

--- config.py
+++ config.py
@@ -1,19 +1,26 @@
+# Warning Filter
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
 import tensorflow as tf
 
 flags = tf.app.flags
 FLAGS = flags.FLAGS
 
-# preprocess use
 flags.DEFINE_string('f', '', 'kernel')
 flags.DEFINE_string('data_path', './dataset/ptb/', 'English Penn Treebank dataset directory')
 
 flags.DEFINE_list('data_file', ['train', 'valid', 'test'], 'PTB dataset lists')
+flags.DEFINE_list('kernel_features', [25, 50, 75, 100, 125, 150], 'kernel size as small data')
+flags.DEFINE_list('kernel_width', [1,2,3,4,5,6], 'kernel width as small data')
 
-flags.DEFINE_integer('dimension', 4, 'choose the character dimension')
-
-# train setup use
+flags.DEFINE_integer('dimension', 15, 'choose the character dimension')
 flags.DEFINE_integer('batch_size', 20, 'DATA-Small batch size')
 flags.DEFINE_integer('trunc_step', 35, 'truncated backpropagation step')
 flags.DEFINE_integer('epoch', 25, 'train epoch size')
+flags.DEFINE_integer('LSTM_hidden', 300, 'number of LSTM hidden units')
+flags.DEFINE_integer('LSTM_layers', 2, 'number of LSTM layers')
+flags.DEFINE_integer('Highway_layers', 1, 'number of Highway layers')
 
 flags.DEFINE_float('learning_rate', 1.0, 'initial learning rate of model')
+flags.DEFINE_float('dropout', 0.5, 'initial dropout of LSTM layers')
---
+++ model.py
@@ -0,0 +1,138 @@
+# Warning Filter
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+import tensorflow as tf
+import easydict
+
+from config import *
+
+def conv2d(input_char, filter_num, filter_width, name="conv2d"):
+    # --------------------------- Input --------------------------- #
+    # input_char : shape of (batch_size * trunc_step, 1, word_maxlen, char_dimension)
+    # filter_num : [25, 50, 75, 100, 125, 150] subscribed in paper
+    # filter_width : [1,2,3,4,5,6] subscribed in paper.
+
+    # --------------------------- Output --------------------------- #
+    # convolution output, filtered with each kernel size and width
+    with tf.variable_scope(name):
+        w = tf.get_variable(name="filters", shape=[1, filter_width, FLAGS.dimension, filter_num])
+        b = tf.get_variable(name='filters_bias', shape=[filter_num])
+
+    return tf.nn.conv2d(input_char, w, strides=[1,1,1,1], padding='VALID') + b
+
+def TDNN(input_embedded, scope='TDNN'):
+    # --------------------------- Input --------------------------- #
+    # input_embedded : shape of (batch_size * trunc_step, word_maxlen, char_dimension)
+    # example : (700, 21, 15)
+
+    # --------------------------- Output --------------------------- #
+    # output : Concatenated version of max-over-time pooling layer
+
+    #word_maxlen = tf.shape(input_embedded)[1]
+    #dimension   = tf.shape(input_embedded)[2]
+    word_maxlen = input_embedded.get_shape()[1]
+    dimension   = input_embedded.get_shape()[2]
+
+    # make channel as 1 with expand_dim
+    input_char = tf.expand_dims(input_embedded, axis=1)
+
+    layers = list()
+    with tf.variable_scope(scope):
+        for kernel_size, kernel_feature_size in zip(FLAGS.kernel_width, FLAGS.kernel_features):
+
+            conv = conv2d(input_char, kernel_feature_size, kernel_size, name="kernel_%d" % kernel_size)
+
+            pool = tf.nn.max_pool(tf.tanh(conv), ksize=(1,1,word_maxlen-kernel_size + 1,1), strides=[1,1,1,1], padding='VALID')
+
+            # get feature map when needed
+            layers.append(tf.squeeze(pool, axis=[1,2]))
+
+        output = tf.concat(layers, axis=1)
+
+    return output
+
+def Affine_Transformation(input_highway, output_dim, scope="AffineTrans"):
+    with tf.variable_scope(scope):
+        W = tf.get_variable(name="highway_matrix", shape=[output_dim, input_highway.get_shape()[1]], dtype=tf.float32)
+        b = tf.get_variable(name="highway_bias", shape=[output_dim], dtype=tf.float32)
+
+    return tf.matmul(input_highway, tf.transpose(W)) + b
+
+def Highway(input_highway, bias=-2.0, scope="Highway"):
+    output_dim = input_highway.get_shape()[1] # 525 - sum of filter numbers
+    with tf.variable_scope(scope):
+        for i in range(FLAGS.Highway_layers):
+            t = tf.sigmoid(Affine_Transformation(input_highway, output_dim, scope='Highway_transgate_%d' % i) + bias)
+            g = tf.nn.relu(Affine_Transformation(input_highway, output_dim, scope='Highway_MLP_%d'%i) + bias)
+
+            z = t * g + (1.0 - t) * input_highway
+
+            if FLAGS.Highway_layers > 1:
+                input_highway = z
+
+    return z
+
+def lstm_cell():
+    cell = tf.contrib.rnn.BasicLSTMCell(FLAGS.LSTM_hidden, forget_bias=0.0, reuse=False)
+    if FLAGS.dropout > 0.0:
+        cell = tf.contrib.rnn.DropoutWrapper(cell, output_keep_prob=1.0 - FLAGS.dropout)
+    return cell
+
+def model_graph(word_maxlen=None, char_size=51, word_size=10000):
+    # --------------------------- Input --------------------------- #
+    # input_embedded : shape of (batch_size * trunc_step, word_maxlen, char_dimension)
+    # example : (700, 21, 15)
+
+    # --------------------------- Output --------------------------- #
+
+    esdict = easydict.EasyDict()
+    input_char = tf.placeholder(tf.int32, shape=[FLAGS.batch_size, FLAGS.trunc_step, word_maxlen], name="input")
+
+    # --------------------------- Embedding Characters --------------------------- #
+    with tf.variable_scope("embedding"):
+        char_embedding = tf.get_variable(name='char_embedding', shape=[char_size, FLAGS.dimension],  dtype=tf.float32)
+
+        char_embedding_padded = tf.scatter_update(ref=char_embedding, indices=[0], \
+                                                  updates=tf.constant(0.0, dtype=tf.float32, shape=[1, FLAGS.dimension]))
+
+        # (batch_size, trunc_step, word_maxlen, char_dimension)
+        input_embedded = tf.nn.embedding_lookup(char_embedding, input_char)
+
+        # (batch_size * trunc_step, word_maxlen, char_dimension)
+        input_embedded = tf.reshape(input_embedded, shape=[-1, word_maxlen, FLAGS.dimension])
+
+    # --------------------------- Convolutional layer --------------------------- #
+    input_highway = TDNN(input_embedded)
+
+    # --------------------------- Highway Network --------------------------- #
+    output_highway = Highway(input_highway, bias=-2.0)
+
+    # --------------------------- LSTM Network --------------------------- #
+    output_highway = tf.reshape(output_highway, shape=[FLAGS.batch_size, FLAGS.trunc_step, -1]) # (20, 35, 525)
+    input_lstm = [tf.squeeze(data, [1]) for data in tf.split(output_highway, FLAGS.trunc_step, axis=1)] # (20, 525) * 35
+
+    stacked_lstm = tf.contrib.rnn.MultiRNNCell([lstm_cell() for _ in range(FLAGS.LSTM_layers)])
+    initial_state = stacked_lstm.zero_state(FLAGS.batch_size, tf.float32)
+
+    outputs, end_state = tf.contrib.rnn.static_rnn(stacked_lstm, inputs=input_lstm, initial_state=initial_state, dtype=tf.float32)
+
+    logits = list()
+    with tf.variable_scope("word_embedding") as scope:
+        for i, output in enumerate(outputs):
+            # output shape : (20, 300)
+            if i != 0:
+                scope.reuse_variables() # not to make new set of variables
+            logits.append(Affine_Transformation(output, word_size))
+
+    esdict.input = input_char
+    esdict.char_embedding_padded = char_embedding_padded
+    esdict.input_embedded = input_embedded
+    esdict.input_highway = input_highway
+    esdict.output_highway = output_highway
+    esdict.lstm_initial_state = initial_state
+    esdict.lstm_end_state = end_state
+    esdict.lstm_outputs = outputs
+    esdict.logits = logits
+
+    return esdict
