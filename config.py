25edbb3e2a116ca8380a69e409e9bc4e6d99f3ef
minstar <wjdalsquf@gmail.com>
2019-02-11 13:45:53 +0900

    perplexity 126.595 with random shuffling

--- config.py
+++ config.py
@@ -9,7 +9,8 @@ FLAGS = flags.FLAGS
 
 flags.DEFINE_string('f', '', 'kernel')
 flags.DEFINE_string('data_path', './dataset/ptb/', 'English Penn Treebank dataset directory')
-flags.DEFINE_string('load_model', './train_dir/epoch23_4.508.model', 'If filename of model parameters is exist, then get the parameters')
+flags.DEFINE_string('load_model', './train_dir/epoch24_4.884.model', 'If filename of model parameters is exist, then get the parameters')
+#flags.DEFINE_string('load_model', None, 'If filename of model parameters is exist, then get the parameters')
 
 flags.DEFINE_list('data_file', ['train', 'valid', 'test'], 'PTB dataset lists')
 flags.DEFINE_list('kernel_features', [25, 50, 75, 100, 125, 150], 'kernel size as small data')
@@ -20,11 +21,11 @@ flags.DEFINE_integer('dimension', 15, 'choose the character dimension')
 flags.DEFINE_integer('batch_size', 20, 'DATA-Small batch size')
 flags.DEFINE_integer('trunc_step', 35, 'truncated backpropagation step')
 flags.DEFINE_integer('epoch', 25, 'train epoch size')
-flags.DEFINE_integer('LSTM_hidden', 300, 'number of LSTM hidden units') # 500
+flags.DEFINE_integer('LSTM_hidden', 500, 'number of LSTM hidden units') # 500
 flags.DEFINE_integer('LSTM_layers', 2, 'number of LSTM layers')
 flags.DEFINE_integer('Highway_layers', 1, 'number of Highway layers')
 flags.DEFINE_integer('verbose', 100, 'number of how many times to print loss')
-flags.DEFINE_integer('total_train', 1, 'set to 1, if train set and validation set needs to be join together')
+flags.DEFINE_integer('total_train', 0, 'set to 1, if train set and validation set needs to be join together')
 
 flags.DEFINE_float('learning_rate', 1.0, 'initial learning rate of model')
 flags.DEFINE_float('dropout', 0.5, 'initial dropout of LSTM layers')
